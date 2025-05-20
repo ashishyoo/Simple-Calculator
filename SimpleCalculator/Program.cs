4a97a0eaf5dfc6c4c076fe57ba34ec7cb4963488
Ashish <ashish.shrestha@truenary.com>
2025-05-20 18:21:14 +0545

    feat: add Menu method

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -21,3 +21,35 @@ void Input()
         Console.Write("Enter second number -> ");
         second = double.Parse(Console.ReadLine());
 }
+
+void Menu()
+{
+        Console.WriteLine("What do you want to do?");
+        Console.WriteLine("[A] Add numbers");
+        Console.WriteLine("[S] Subtract numbers");
+        Console.WriteLine("[M] Multiply numbers");
+        Console.Write("Choice -> ");
+        string? choice = Console.ReadLine();
+        double result = 0;
+        if (choice == "A" || choice == "a")
+        {
+                result = first + second;
+                Console.WriteLine($"{first} + {second} = {result:F2}");
+        }
+        else if (choice == "S" || choice == "s")
+        {
+                result = first - second;
+                Console.WriteLine($"{first} - {second} = {result:F2}");
+        }
+        else if (choice == "M" || choice == "m")
+        {
+                result = first * second;
+                Console.WriteLine($"{first} * {second} = {result:F2}");
+        }
+        else
+        {
+                Console.WriteLine("Invalid choice!");
+                Console.WriteLine("Press any key to close.");
+                Console.ReadKey();
+        }
+}
